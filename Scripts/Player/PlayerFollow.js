3b83782bb77269f1e1f2ffe58598f59de618a0c0
Topher <tophermade@gmail.com>
2016-03-25 01:39:42 -0600

    starting fast

---
+++ Scripts/Player/PlayerFollow.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+//GameObject / AudioClip / Transform
+var focusObject			: GameObject;
+
+// boolean
+var initial 			: Vector3;
+
+
+// float / int
+var damp 				: float 		= .1;
+var velocity		 	: Vector3		= Vector3.zero;
+
+
+// String
+
+
+//StandardFunction
+function Awake(){
+	initial = transform.position;
+}
+
+function Start(){
+
+}
+
+function Update(){
+	transform.position = Vector3(focusObject.transform.position.x+initial.x, focusObject.transform.position.y + initial.y-1.4, focusObject.transform.position.z + initial.z);
+}
+
+function FixedUpdate(){
+
+}
---
+++ Scripts/Player/PlayerMovement.js
@@ -0,0 +1,75 @@
+﻿#pragma strict
+
+var manager 		: GameObject;
+var heightIndicator : GameObject;
+var body 			: Rigidbody;
+
+
+var speed 			: float			= 5;
+var jumpPower 		: float 		= 9;
+
+
+var playing 		: boolean 		= false;
+var onGround 		: boolean 		= false;
+
+
+
+
+function HexToColor(hex : String, alpha : int) : Color{
+    var r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+    var g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+    var b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+    return new Color32(r,g,b, alpha);
+}
+
+
+function HitWall(){
+
+}
+
+
+function OnCollisionEnter(collision : Collision) {
+	var hitWall : boolean = false;
+
+	for (var contact : ContactPoint in collision.contacts) {
+		if(heightIndicator.transform.position.y < contact.point.y){
+			print("Hit a wall");
+			hitWall = true;
+		} else {
+			print("Landed on the ground");
+			onGround = true;
+		}
+	}
+
+	if(hitWall){
+		HitWall();
+	}
+}
+
+
+function StartRound(){
+	playing = true;
+}
+
+
+
+function Start () {
+	body = GetComponent(Rigidbody);
+	StartRound();
+}
+
+
+
+function FixedUpdate(){
+	if(playing){
+		body.velocity.x = speed;
+	}
+}
+
+
+
+function Update () {
+	if(Input.GetMouseButtonDown(0)){
+		body.velocity.y = jumpPower;
+	}
+}
